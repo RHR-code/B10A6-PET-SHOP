ec1590054b9606eee81d1da6234cb696200383b9
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-08-09 19:58:04 +0600

    project almost done but there lot of things can be done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+let body = document.querySelector("body");
 let allPetsLeftSide = document.querySelector(".allPetsLeftSide");
 let loader = document.querySelector(".loader");
 let categories = document.querySelector(".category");
@@ -6,9 +7,12 @@ let favouritePet = document.querySelector(".favouritePet");
 let petsArray;
 let sortedAnimal;
 let adoptModal = document.querySelector(".adoptModal");
+let detailsModal = document.querySelector(".detailsModal");
+let loaderDiv = document.querySelector(".loaderDiv");
 
 function addPets(pets) {
   allPetsLeftSide.innerHTML += `
+  
       <div id="${pets.petId}" class="pet p-5 border border-gray-400 rounded-lg">
                 <img class="mb-5" src=${pets.image} alt="" />
                 <h6 class="font-bold text-xl mb-3">${pets.pet_name}</h6>
@@ -66,7 +70,11 @@ let allPets = async () => {
     loader.style.display = "none";
   }
 };
-window.onload = allPets();
+
+window.addEventListener("load", () => {
+  allPets();
+});
+// window.onload = allPets();
 // getting all the category
 let category = async () => {
   let getCategory = await fetch(
@@ -78,10 +86,10 @@ let category = async () => {
     categories.innerHTML += `
   <div
             id-"${item.id}"
-            class="flex cursor-pointer active:bg-[rgb(14,122,129,.2)] justify-center h-[104px] items-center w-[312px] gap-4  rounded-full border border-[#0E7A81]"
+            class="flex cursor-pointer active:bg-[rgb(14,122,129,.2)] justify-center h-[50px] md:h-[104px] items-center w-[100px] md:w-[312px] gap-2 md:gap-4  rounded-full border border-[#0E7A81]"
           >
-            <img src=${item.category_icon} alt="" />
-            <h4 class="font-bold text-2xl">${item.category}</h4>
+            <img class="w-[20px] md:w-auto" src=${item.category_icon} alt="" />
+            <h4 class="font-bold text-lg md:text-2xl">${item.category}</h4>
 
           </div>
   `;
@@ -108,12 +116,12 @@ let category = async () => {
         let getAnimals = await fetch(
           `https://openapi.programming-hero.com/api/peddy/category/${cateName}`
         );
-
         let animalsData = await getAnimals.json();
         sortedAnimal = animalsData.data;
         if (animalsData.data.length !== 0) {
           sortedAnimal.map((pets) => {
             addPets(pets);
+            likeAdoptDetails();
           });
         } else {
           allPetsLeftSide.innerHTML = `
@@ -133,21 +141,33 @@ window.onload = category();
 
 // sorting pets by price
 let sortingPets = async () => {
-  petsArray.sort(function (a, b) {
-    return b.price - a.price;
-  });
-  allPetsLeftSide.innerHTML = "";
-  petsArray.forEach((pet) => {
-    addPets(pet);
-  });
+  loader.style.display = "flex";
+  allPetsLeftSide.classList.add("hidden");
+  try {
+    await new Promise((res) => {
+      setTimeout(res, 2000);
+    });
+    allPetsLeftSide.classList.toggle("hidden");
+    petsArray.sort(function (a, b) {
+      return b.price - a.price;
+    });
+    allPetsLeftSide.innerHTML = "";
+    petsArray.forEach((pet) => {
+      addPets(pet);
+    });
 
-  sortedAnimal.sort(function (a, b) {
-    return b.price - a.price;
-  });
-  allPetsLeftSide.innerHTML = "";
-  sortedAnimal.forEach((pet) => {
-    addPets(pet);
-  });
+    sortedAnimal.sort(function (a, b) {
+      return b.price - a.price;
+    });
+    allPetsLeftSide.innerHTML = "";
+    sortedAnimal.forEach((pet) => {
+      addPets(pet);
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.style.display = "none";
+  }
 };
 
 sortBtn.addEventListener("click", sortingPets);
@@ -159,6 +179,7 @@ function likeAdoptDetails() {
 
   addLike(like);
   addAdopt(adopt);
+  addDetails(details);
 }
 
 function addLike(likes) {
@@ -223,6 +244,59 @@ function addAdopt(adopt) {
           }
         }, (3 - i) * 1000);
       }
+    });
+  });
+}
+
+function addDetails(detail) {
+  detail.forEach((info) => {
+    info.addEventListener("click", async (e) => {
+      let currentId = e.target.closest(".pet").id;
+      let getPet = await fetch(
+        `https://openapi.programming-hero.com/api/peddy/pet/${currentId}`
+      );
+      let petData = await getPet.json();
+      let pets = petData.petData;
+      detailsModal.classList.toggle("hidden");
+      console.log([pets]);
+      detailsModal.innerHTML = `
+      <div
+          class="fixed inset-0 bg-black/50 h-full flex justify-center items-center "
+        >
+       <div id="${pets.petId}" class="pet p-5 border border-gray-400 rounded-lg bg-white w-2xl flex flex-col items-center h-[70%] overflow-auto">
+                <img class="mb-5 w-[80%]" src=${pets.image} alt="" />
+                <h6 class="font-bold text-xl mb-3">${pets.pet_name}</h6>
+                <p class="flex mb-2">
+                  <img class="w-5 mr-2" src="../images/card.png" alt="" /> Breed:
+                  ${pets.breed}
+                </p>
+                <p class="flex mb-2">
+                  <img class="w-5 mr-2" src="../images/calendar.png" alt="" />
+                  Birth: ${pets.date_of_birth}
+                </p>
+                <p class="flex mb-2">
+                  <img class="w-5 mr-2" src="../images/femenine.png" alt="" />
+                  Gender: ${pets.gender}
+                </p>
+                <p class="flex mb-2">
+                  <img
+                    class="w-5 mr-2"
+                    src="../images/dollar-sign.png"
+                    alt=""
+                  />Price : ${pets.price}$
+                </p>
+                <p>${pets.pet_details}</p>
+                <button class="detailCloseBtn bg-emerald-400 text-white font-bold text-xl w-full h-[40px] rounded-xl mt-3">Close</button>
+              </div>
+              </div>
+
+      `;
+      body.classList.add("overflow-hidden");
+      let detailBtn = document.querySelector(".detailCloseBtn");
+      detailBtn.addEventListener("click", () => {
+        detailsModal.classList.toggle("hidden");
+        body.classList.toggle("overflow-hidden");
+      });
     });
   });
 }
